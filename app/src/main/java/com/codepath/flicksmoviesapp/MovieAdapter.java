6347b2b61a830f69fecab9c09555cbf88af8f08d
Taylor Jean-Jacques <tjeanjacques@fb.com>
2017-06-22 13:58:51 -0700

    fixed sceen layout size

--- app/src/main/java/com/codepath/flicksmoviesapp/MovieAdapter.java
+++ app/src/main/java/com/codepath/flicksmoviesapp/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.flicksmoviesapp;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -51,13 +52,29 @@ Movie movie = movies.get(position);
         holder.tvTitle.setText(movie.getTitle());
         holder.tvOverview.setText(movie.getOverview());
 
-        String imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        //determine current orientation
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
+        String imageUrl = null;
+
+        if(isPortrait){
+            imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else{
+            imageUrl = config.getImageUrl(config.getBackdropSize(), movie.getBackdropPath());
+
+        }
+
+        //get placeholder/image view
+        int placeholderId = isPortrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? holder.ivPosterImage : holder.ivBackdropImage;
+
+
         Glide.with(context)
                 .load(imageUrl)
                 .bitmapTransform(new RoundedCornersTransformation(context, 25, 0))
-                .placeholder(R.drawable.flicks_movie_placeholder)
-                .error(R.drawable.flicks_movie_placeholder)
-                .into(holder.ivPosterImage);
+                .placeholder(placeholderId)
+                .error(placeholderId)
+                .into(imageView);
     }
 
 
@@ -69,12 +86,15 @@ Movie movie = movies.get(position);
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
 ImageView ivPosterImage;
+        ImageView ivBackdropImage;
         TextView tvTitle;
         TextView tvOverview;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
             ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
+            ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
             tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
             tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
 
--- app/src/main/java/com/codepath/flicksmoviesapp/models/Config.java
+++ app/src/main/java/com/codepath/flicksmoviesapp/models/Config.java
@@ -35,4 +35,8 @@ public class Config {
     public String getPosterSize() {
         return posterSize;
     }
+
+    public String getBackdropSize() {
+        return backdropSize;
+    }
 }
